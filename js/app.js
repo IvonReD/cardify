8224eb76bd010f607aef5e4b3d927e270c3b711f
IvonReD <ivonne.reyes.dc@gmail.com>
2018-02-28 23:42:46 -0600

    Subiendo funcionalidad del proyecto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-Escribe tu código aqui----
+//Escribe tu código aqui----
+
+const gallery = document.getElementById('gallery')
+var images = document.querySelectorAll('img');
+
+const insertElements = (figure, figCaption, infoAlt) => {
+    gallery.appendChild(figure);
+    figure.appendChild(infoAlt);
+    figure.appendChild(figCaption);
+    figCaption.innerText = infoAlt.alt;
+}
+
+const createElements = infoAlt => {
+    let figure = document.createElement('figure');
+    figure.className = 'img';
+    let figCaption = document.createElement('figcaption');
+    insertElements(figure, figCaption, infoAlt);
+}
+
+Array.from(images).forEach(image =>{
+    let infoAlt = image.cloneNode();
+    createElements(infoAlt);
+    gallery.removeChild(image);
+});
